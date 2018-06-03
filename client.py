1a716eca4d743efb495bd3e5f28940e5b02be0f1
Hacking for fun~ <330514376@qq.com>
2018-06-03 17:27:49 +0800

    a

--- client.py
+++ client.py
@@ -4,29 +4,31 @@ import sys  # for exit
 # create an AF_INET, STREAM socket (TCP)
 def a():
     #remote
-    host = '127.0.0.1'
-    #host = '139.199.223.182'
+    #host = '127.0.0.1'
+    host = '139.199.223.182'
     port = 8080
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     remote_ip = socket.gethostbyname( host )
     s.connect((remote_ip,port))
-    b=s.recv(4096)
 
+    b=s.recv(40960)
+    print(b)
     #local
     reply=sendlocal(b)
     s.sendall(reply)
-    print(s)
+    print(reply)
 
 
 
 def sendlocal(b):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect(('127.0.0.1',80))
+    s.connect(('127.0.0.1',4000))
     s.sendall(b)
-    reply = s.recv(4096)
+    reply = s.recv(409600)
     return reply
 
-a()
+while 1:
+    a()
 
 
 
--- server.py
+++ server.py
@@ -4,49 +4,60 @@ import time
 import threading
 from multiprocessing import Queue
 
-#接受本机
-def listen1(p,Q):
+#8080
+def listen1(p,QGET,QRET):
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.bind(('0.0.0.0', p))
     sock.listen(5)
     while True:
-        conn, address = sock.accept()
-        #buf=conn.recv(1024)
-        print('wait.....')
-        E.wait()
-        print('wait End...')
-        conn.send(Q.get())
+        try:
+            conn, address = sock.accept()
+            #GET msg
+            msg=QGET.get()
+            conn.send(msg)
+            conn.settimeout(5)
+            print('server wait client..:')
+            buf = conn.recv(409600)
+            print('server'+str(p)+' rec:'+str(buf))
+            QRET.put(buf)
+            conn.close()
+        except Exception as e:
+            print(e)
+            continue
+#80
+def listen2(p,QGET,QRET):
 
-        buf = conn.recv(1024)
-        print('server rec:'+str(buf))
-        Q.put(buf)
-
-
-
-
-
-def listen2(p,Q):
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.bind(('0.0.0.0', p))
     sock.listen(5)
     while True:
-        conn, address = sock.accept()
-        buf = conn.recv(1024)
-        print(str(p)+'shoudao'+str(buf))
-        Q.put(buf)
-        E.set()
-        time.sleep(5)
-        buf=Q.get()
-        print(buf)
-        conn.send(buf)
+        try:
+            print('wating1.....')
+            conn, address = sock.accept()
+            buf = conn.recv(409600)
+            print('sertver Rece'+str(p)+str(buf))
+            #传给8080
+            QGET.put(buf)
 
+            print('wating.....')
+            buf=QRET.get()
+            print('wating end return http...')
+            conn.send(buf)
+            print('sendok')
+            conn.close()
+        except Exception as e:
+            print(e)
+            continue
 E = threading.Event()
-Q = Queue()
+#外网的htpp包
+QGET = Queue()
+#从内网返回的包
+QRET = Queue()
 
 #接受本机
-t1=threading.Thread(target=listen1,args=(8080,Q,))
+t1=threading.Thread(target=listen1,args=(8080,QGET,QRET))
 t1.start()
 
 #接受访问者
-t2=threading.Thread(target=listen2,args=(801,Q))
+t2=threading.Thread(target=listen2,args=(80,QGET,QRET))
 t2.start()
